7e6acf1539bc355d7c69f4460665315f0769e6e0
chris-kc-cheng <kc1116@gmail.com>
2023-12-06 06:15:43 -0500

    Added OAuth2

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ftk-api
-Backend of the Financial Toolkit using FastAPI
+# Backend of the Financial Toolkit using FastAPI
+
+Interactive API docs by Swagger UI is hosted at Render cloud at:\
+https://ftk-api.onrender.com/docs
+
+Alternative API docs by ReDoc:\
+https://ftk-api.onrender.com/redoc
+
+Note: The back-end is currently hosted on Render cloud under the free-tier so it may take a minute or two to wake up after 15 minutes of inactivity.
--- main.py
+++ main.py
@@ -1,8 +1,144 @@
-from fastapi import FastAPI
+# Standard
+import os
+from datetime import date, datetime, timedelta
+from enum import Enum
+from typing_extensions import Annotated
 
-app = FastAPI()
+# FastAPI/Pydantic
+from fastapi import FastAPI, Body, Depends, Query, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import ConfigDict, BaseModel, Field, EmailStr
+from pydantic.functional_validators import BeforeValidator
+
+# Security
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+
+# Database
+from bson import ObjectId
+import motor.motor_asyncio
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+app = FastAPI(
+    title="Financial Toolkit API",
+    summary="Backend of the Financial Toolkit Demo",
+)
+
+client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_HOST"])
+db = client.ftkdb
+
+PyObjectId = Annotated[str, BeforeValidator(str)]
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {'message': 'success'}
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+
+
+class User(BaseModel):
+    id: PyObjectId = Field(alias="_id", default=None)
+    username: str
+    password: str
+    first_name: str = Field(alias="firstName")
+    last_name: str = Field(alias="lastName")
+    is_active: bool = Field(alias="isActive")
+    roles: list[str]
+
+    model_config = ConfigDict(
+        populate_by_name=True)
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+async def get_user(username: str):
+    try:
+        user = await db.user.find_one({'username': username, 'isActive': True})
+        return User(**user)
+    except Exception as e:
+        return False
+
+
+async def authenticate_user(username: str, password: str):
+    user = await get_user(username)
+    if not user:
+        return False
+    if not verify_password(password, user.password):
+        return False
+    return user
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
+    user = await authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = await get_user(token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+@app.get("/users/me/", response_model=User)
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_user)]
+):
+    return current_user
